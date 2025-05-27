953d8314240ebc2812fbc99f9c5e849d499f8dd1
Sarah Stec <74212084+sastec17@users.noreply.github.com>
2025-05-27 11:40:45 -0400

    Update setup.py - Add LangChainKaltura

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "xlrd",
         "markdown",
         "pytz",
+        "LangChainKaltura @ git+https://github.com/tl-its-umich-edu/langchain_kaltura@2025.03.01",
     ],
     python_requires=">=3.8.1",
 )
